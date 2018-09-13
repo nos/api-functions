dae0e14cc9d259d01327e90ba1b7eef0e6ed49de
Jeroen Peeters <jeroen.peeters@evance.be>
2018-09-13 22:20:53 +0200

    feat: add getLastBlock
    also refactored es6 to src

--- src/nos.js
+++ src/nos.js
@@ -7,27 +7,34 @@ import executor from "./executor";
  * @function getAddress
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const getAddress = fallback => executor('getAddress', fallback);
+const getAddress = fallback => executor("getAddress", fallback);
 
 /**
  * @function getBalance
  * @param {object} config All possible arguments and flags are passed as childs of this object
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const getBalance = (config, fallback) => executor('getBalance', fallback, config);
+const getBalance = (config, fallback) => executor("getBalance", fallback, config);
+
+/**
+ * @function getLastBlock
+ * @param {object} config All possible arguments and flags are passed as childs of this object
+ * @param {function} fallback fallback function if outside of nOS browser
+ */
+const getLastBlock = (config, fallback) => executor("getLastBlock", fallback, config);
 
 /**
  * @function claimGas
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const claimGas = fallback => executor('claimGas', fallback);
+const claimGas = fallback => executor("claimGas", fallback);
 
 /**
  * @function send
  * @param {object} config All possible arguments and flags are passed as childs of this object
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const send = (config, fallback) => executor('send', fallback, config);
+const send = (config, fallback) => executor("send", fallback, config);
 
 // Smart Contract Actions
 
@@ -36,27 +43,28 @@ const send = (config, fallback) => executor('send', fallback, config);
  * @param {object} config All possible arguments and flags are passed as childs of this object
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const testInvoke = (config, fallback) => executor('testInvoke', fallback, config);
+const testInvoke = (config, fallback) => executor("testInvoke", fallback, config);
 
 /**
  * @function invoke
  * @param {object} config All possible arguments and flags are passed as childs of this object
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const invoke = (config, fallback) => executor('invoke', fallback, config);
+const invoke = (config, fallback) => executor("invoke", fallback, config);
 
 /**
  * @function getStorage
  * @param {object} config All possible arguments and flags are passed as childs of this object
  * @param {function} fallback fallback function if outside of nOS browser
  */
-const getStorage = (config, fallback) => executor('getStorage', fallback, config);
+const getStorage = (config, fallback) => executor("getStorage", fallback, config);
 
 const nos = {
   claimGas,
   exists,
   getAddress,
   getBalance,
+  getLastBlock,
   getStorage,
   invoke,
   send,
